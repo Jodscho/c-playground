e25dd854e603ca2d23104e5e88eebd5306a24ac5
Jodscho <Jonathan2008@gmx.de>
2021-03-20 23:26:01 +0100

    more matrix functionality

--- matrix-util/main.c
+++ matrix-util/main.c
@@ -1,16 +1,22 @@
 #include "matrix.h"
+#include <stdio.h>
 
 int main() {
 
-    double vals1[] = {1,5,3,2};
-    double vals2[] = {1,2,3,4,5,6};
-    struct matrix* m1 = create(2, 2, vals1);
-    struct matrix* m2 = create(2, 3, vals2);
+    double vals1[] = {2,-1,0,-1,2,-1,0,-1,2};
+    double vals2[] = {1,2,3,4};
+    struct matrix* m1 = create(3, 3, vals1);
+    struct matrix* m2 = create(2, 2, vals2);
 
     print(m1);
-    print(m2);
-    print(multiply(m1, m2));
-    print(transpose(m1));
-    print(add(m1, transpose(m1)));
+    //print(m2);
+    //print(multiply(m1, m2));
+    //print(transpose(m1));
+    //print(add(m1, transpose(m1)));
+    //print(identity(5));
+    //print(identity(5));
+    //printf("%f\n",determinant(m1));
+    print(inverse(m1));
+    print(inverse(m2));
 
 }
--- matrix-util/matrix.c
+++ matrix-util/matrix.c
@@ -16,6 +16,82 @@ void freem(struct matrix* m){
    free(m->values); 
 }
 
+struct matrix* identity(int rows){
+    double values[rows*rows];
+    for(int i = 0; i < rows*rows; i++){
+        values[i] = (i % (rows+1) == 0) ? 1 : 0;
+    }
+    return create(rows, rows, values);
+}
+
+struct matrix* inverse(struct matrix* m){
+
+    if(m->rows != m->cols) {
+        fprintf(stderr, "matrix needs to be symmetric");
+        exit(1);
+    }
+    
+    if(m->rows > 3){
+        fprintf(stderr, "inverse can only be computed for dimensions <= 3");
+        exit(1);
+    }
+
+    double factor = 1/determinant(m);
+    double values[m->rows*m->cols];
+
+    if(m->rows == 3){
+        double a = at(m,0,0);
+        double b = at(m,0,1);
+        double c = at(m,0,2);
+        double d = at(m,1,0);
+        double e = at(m,1,1);
+        double f = at(m,1,2);
+        double g = at(m,2,0);
+        double h = at(m,2,1);
+        double i = at(m,2,2);
+        values[0] = (e*i-f*h)*factor;
+        values[1] = (c*h-b*i)*factor;
+        values[2] = (b*f-c*e)*factor;
+        values[3] = (f*g-d*i)*factor;
+        values[4] = (a*i-c*g)*factor;
+        values[5] = (c*d-a*f)*factor;
+        values[6] = (d*h-e*g)*factor;
+        values[7] = (b*g-a*h)*factor;
+        values[8] = (a*e-b*d)*factor;
+    } else if(m->rows == 2){
+        values[0] = at(m,1,1)*factor;
+        values[1] = -1*at(m,0,1)*factor;
+        values[2] = -1*at(m,1,0)*factor;
+        values[3] = at(m,0,0)*factor; 
+    }
+    return create(m->rows, m->cols, values);
+}
+
+
+double determinant(struct matrix* m){
+
+    if(m->rows != m->cols) {
+        fprintf(stderr, "matrix needs to be symmetric");
+        exit(1);
+    }
+    
+    if(m->rows > 3){
+        fprintf(stderr, "determinant can only be computed for dimensions <= 3");
+        exit(1);
+    }
+
+    if(m->rows == 2){
+        return at(m, 0, 0) * at(m, 1, 1) - at(m, 1, 0) * at(m, 0, 1);
+    } else if(m->rows == 3){
+        return  at(m,0,0)*at(m,1,1)*at(m,2,2)
+              + at(m,0,1)*at(m,1,2)*at(m,2,0)
+              + at(m,0,2)*at(m,1,0)*at(m,2,1)
+              - at(m,2,0)*at(m,1,1)*at(m,0,2)
+              - at(m,2,1)*at(m,1,2)*at(m,0,0)
+              - at(m,2,2)*at(m,1,0)*at(m,0,1);
+    }
+}
+
 struct matrix* create(int rows, int cols, double* vals){
     if(rows < 0 || cols < 0) {
         fprintf(stderr, "negative value for rows/cols");
